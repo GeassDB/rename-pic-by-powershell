5d0f983d88361eb4bca4b083eb5a18d47ea460f8
GeassDB <geassdb@outlook.com>
2015-08-05 23:44:51 +0800

    renpic

---
+++ renpic.ps1
@@ -0,0 +1,14 @@
+﻿$i = 0
+$file = "main,jpg"
+$x = Split-Path -Parent $MyInvocation.MyCommand.Definition
+#Dir $x -recurse -filter *.jpg  |
+Get-ChildItem -Path $x -Filter *.jpg |
+
+ForEach-Object { 
+$extension = $_.Extension
+$newName = '{0:d6}{1}' -f $i, $extension
+$i++
+if($_.Name -ne "main.jpg"){
+Rename-Item -Path $_.FullName -NewName $newName
+}
+}
